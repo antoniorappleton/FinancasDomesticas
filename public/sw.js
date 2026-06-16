be5b85cd475f0ef51824cb85c1908204f5681d99
Seu Nome <seuemail@exemplo.com>
2026-06-16 14:12:29 +0100

    +Nova

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const VERSION = "v159";
+const VERSION = "v162";
 
 // Base do scope: ex. "https://user.github.io/REPO/" -> "/REPO"
 const BASE_PATH = new URL(self.registration.scope).pathname.replace(/\/$/, "");
